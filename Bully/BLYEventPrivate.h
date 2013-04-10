0c27cda263981907303b019111bb19ffd55c855a
Skylar Schipper <ss@schipp.co>
2013-04-10 09:47:58 -0700

    Import BLYEvent in Bully.h

--- Bully/BLYEventPrivate.h
+++ Bully/BLYEventPrivate.h
@@ -6,9 +6,10 @@
 //  Copyright (c) 2013 Sam Soffes. All rights reserved.
 //
 
-#ifndef Bully_BLYEventPrivate_h
-#define Bully_BLYEventPrivate_h
+#import "BLYEvent.h"
 
+@interface BLYEvent ()
 
+- (NSURLRequest *)eventRequestWithAppID:(NSString *)appID key:(NSString *)key secret:(NSString *)secret;
 
-#endif
+@end
