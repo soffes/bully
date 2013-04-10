9e1a2b39c95c69417615951b44b0af081effcd48
Skylar Schipper <ss@schipp.co>
2013-04-10 09:47:42 -0700

    Added private header for BLYEvent

---
+++ Bully/BLYEventPrivate.h
@@ -0,0 +1,14 @@
+//
+//  BLYEventPrivate.h
+//  Bully
+//
+//  Created by Skylar Schipper on 4/10/13.
+//  Copyright (c) 2013 Sam Soffes. All rights reserved.
+//
+
+#ifndef Bully_BLYEventPrivate_h
+#define Bully_BLYEventPrivate_h
+
+
+
+#endif
